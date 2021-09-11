daad5858bb5a8efb91c91287874f41978078b71e
Gulsaba <89983752+Gulsaba@users.noreply.github.com>
2021-09-11 13:56:19 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+var TinyURL = require('tinyurl');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:false}));
+app.get("/", function(req, res){
+  res.render("index");
+})
+// Insertion of urs.css file where path is views>css>url.css
+
+app.use(express.static('views'));
+
+app.post('/signup', function(req, res){
+  res.send("<h2> You have resistered successfully...</h2>");
+ 
+})
+app.post("/login", function(req, res){
+  res.render("index");
+})
+app.post("/shortenUrl", function(req, res){
+ 
+  var url = req.body.fullUrl;
+   
+  TinyURL.shorten(url, function(result, error) {
+    if (error)
+    res.send(error)
+
+    res.send(result);
+  
+  });
+  //res.redirect('/');
+})
+
+app.listen(3000, function(){
+    console.log("This server strat from port 3000");
+});
